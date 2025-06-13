09aa9a5756055588f56cefd21a9fbd8fcff70e54
SongYoungMin <dudals789@naver.com>
2025-06-13 23:39:08 +0900

    first commit

---
+++ src/main/java/com/project/flutterbackendapi/FlutterBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.flutterbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlutterBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/flutterbackendapi/FlutterBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flutterbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

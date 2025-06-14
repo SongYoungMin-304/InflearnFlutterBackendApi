d6c5a586428565377d12b345f80e5e88c5ad47d9
SongYoungMin <dudals789@naver.com>
2025-06-14 14:53:39 +0900

    - chapter2 관련 커밋

---
+++ src/main/java/com/project/flutterbackendapi/entity/Test.java
@@ -0,0 +1,25 @@
+package com.project.flutterbackendapi.entity;
+
+import com.project.flutterbackendapi.model.response.TestResponseDTO;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="test")
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "test_id")
+    private Long id;
+
+    @Column(name = "test_name")
+    private String testName;
+
+
+    public TestResponseDTO toResponseDTO() {
+        return TestResponseDTO.builder()
+                .id(id)
+                .name(testName)
+                .build();
+    }
+}
--- src/main/java/com/project/flutterbackendapi/service/TestService.java
+++ src/main/java/com/project/flutterbackendapi/service/TestService.java
@@ -1,4 +1,26 @@
 package com.project.flutterbackendapi.service;
 
+import com.project.flutterbackendapi.entity.Test;
+import com.project.flutterbackendapi.repository.TestRepository;
+import com.project.flutterbackendapi.repository.queryDsl.TestQueryDslRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class TestService {
+    private final TestRepository testRepository;
+
+    private final TestQueryDslRepository testQueryDslRepository;
+
+    public Test findById(Long id) {
+        return testRepository.findById(id).orElseThrow(() -> new RuntimeException("Test not found with id: " + id));
+    }
+
+    public Test findV2ById(Long id) {
+        return testQueryDslRepository.findById(id).orElseThrow(() -> new RuntimeException("Test not found with id: " + id));
+    }
+
 }
